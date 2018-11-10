9d4f8880ffc08c601fb388359013b0471cb5f4fd
samihda <samihda@users.noreply.github.com>
2018-11-10 21:39:06 +0100

    Install Moodle

---
+++ web/config.php
@@ -0,0 +1,31 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$db = parse_url(getenv("DATABASE_URL"));
+
+$CFG->dbtype    = 'pgsql';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = $db["host"];
+$CFG->dbname    = ltrim($db["path"], "/");
+$CFG->dbuser    = $db["user"];
+$CFG->dbpass    = $db["pass"];
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => $db["port"],
+  'dbsocket' => '',
+);
+
+$CFG->wwwroot   = 'http://olwyn-moodle-alt.herokuapp.com';
+$CFG->dataroot  = '/app/moodledata';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 02777;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
